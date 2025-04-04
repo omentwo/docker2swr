edd04eba73dbdab9a259d9faf8a48fd04cb494c1
omentwo <131430896+omentwo@users.noreply.github.com>
2025-04-04 22:23:45 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import WindICSS from 'vite-plugin-windicss'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [vue(), WindICSS()],
-})
